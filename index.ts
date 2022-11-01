2570cdb31ba64d43bfe8d45fd44e4595eeb343ac
Marco Antonio <mkchoque@gmail.com>
2022-10-31 21:22:16 -0400

    configuracion de cors

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import Server from './classes/server';
 import router from "./routes/router";
 import bodyParser from "body-parser";
+import cors from 'cors';
 
 
 const server = new Server();
@@ -12,6 +13,9 @@ const server = new Server();
 server.app.use( bodyParser.urlencoded({ extended: true }) );
 server.app.use( bodyParser.json() );
 
+
+//CORS
+server.app.use( cors({origin: true, credentials: true }) );
 
 
 
